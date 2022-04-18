da360ecb2a39621435f8747149dedb5359f1561d
Alan Henry <alandavidhenry@gmail.com>
2022-04-18 21:49:55 +0100

    Part of Section 43: Errors & Validating Data (447)

--- routes/campground.js
+++ routes/campground.js
@@ -1,45 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+
 const Campground = require('../models/campground');
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     res.render('home')
 });
-router.get('/campgrounds', async (req, res) => {
+router.get('/campgrounds', catchAsync(async (req, res, next) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
-});
-router.get('/campgrounds/new', (req, res) => {
+}));
+
+router.get('/campgrounds/new', (req, res, next) => {
     res.render('campgrounds/new');
 });
 
-router.post('/campgrounds', async (req, res) => {
+router.post('/campgrounds', async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 });
 
-router.get('/campgrounds/:id', async (req, res,) => {
+router.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
-});
+}));
 
-router.get('/campgrounds/:id/edit', async (req, res) => {
+router.get('/campgrounds/:id/edit', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-});
+}));
 
-router.put('/campgrounds/:id', async (req, res) => {
+router.put('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-});
+}));
 
-router.delete('/campgrounds/:id', async (req, res) => {
+router.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}));
+
+router.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
 });
 
 module.exports = router;
