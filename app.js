d07ab6f632e7624a1c362d0e3d516ed8c0e5daf8
Alan Henry <alandavidhenry@gmail.com>
2022-04-22 23:11:50 +0100

    Up to end of Section 43: Errors & Validating Data

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+
 const Campground = require('./models/campground');
 
 // var indexRouter = require('./routes/index');
@@ -39,20 +40,20 @@ app.use(methodOverride('_method'));
 // app.use('/users', usersRouter);
 app.use('/', campgroundRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
--- routes/campground.js
+++ routes/campground.js
@@ -5,49 +5,77 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 
 const Campground = require('../models/campground');
+const { campgroundSchema } = require('../schemas');
 
+const validateCampground = (req, res, next) => {
+    const result = campgroundSchema.validate(req.body);
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+/* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('home')
 });
+
+/* GET campgrounds index page. */
 router.get('/campgrounds', catchAsync(async (req, res, next) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 }));
 
+/* GET new campground page. CREATE */
 router.get('/campgrounds/new', (req, res, next) => {
     res.render('campgrounds/new');
 });
 
-router.post('/campgrounds', async (req, res, next) => {
+/* POST new campground page. CREATE */
+router.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-});
+}));
 
+/* GET campground using id page. READ */
 router.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
 }));
 
+/* GET campground edit page. */
 router.get('/campgrounds/:id/edit', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
 }));
 
-router.put('/campgrounds/:id', catchAsync(async (req, res, next) => {
+/* PUT campground edit page. UPDATE */
+router.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
+/* DELETE campground delete page. DELETE */
 router.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }));
 
+/* 404 for any incorrect page. */
 router.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
+});
+
+router.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err });
 });
 
 module.exports = router;
