42c881021dbcdfd4e0d75c10b52c3b39a49a779a
Alan Henry <alandavidhenry@gmail.com>
2022-04-24 11:38:30 +0100

    Up to end of Section 49: Restructuring & Flash

--- routes/campground.js
+++ routes/campground.js
@@ -40,12 +40,20 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 /* GET campground using id page. READ */
 router.get('/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+        req.flash('error', 'Campground does not exist');
+        res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
 }));
 
 /* GET campground edit page. */
 router.get('/:id/edit', catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id);
+    if(!campground){
+        req.flash('error', 'Campground does not exist');
+        res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground });
 }));
 
@@ -60,6 +68,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res, next) => {
 router.delete('/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground');
     res.redirect('/campgrounds');
 }));
 
