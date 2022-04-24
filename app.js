be7e5c5799bf20426bd41ec3100948ba351a1fac
Alan Henry <alandavidhenry@gmail.com>
2022-04-24 10:20:06 +0100

    Part of Section 49: Restructuring & Flash

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
 
-const Campground = require('./models/campground');
-
-// var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 const campgroundRouter = require('./routes/campground');
+const reviewRouter = require('./routes/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -36,11 +38,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(methodOverride('_method'));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+const sessionConfig = {
+    secret: 'badsecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + (1000 * 60 * 60 * 24 * 7),
+        maxAge: (1000 * 60 * 60 * 24 * 7)
+    }
+}
+app.use(session(sessionConfig));
 
-// app.use('/campgrounds'); USING THIS MEANS CAN REMOVE campgrounds FROM ROUTES IN campground.js
-app.use('/', campgroundRouter);
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.use('/campgrounds', campgroundRouter);
+app.use('/campgrounds/:id/reviews', reviewRouter);
+
+/* 404 for any incorrect page. */
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err });
+});
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
--- routes/campground.js
+++ routes/campground.js
@@ -5,8 +5,7 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 
 const Campground = require('../models/campground');
-const { campgroundSchema, reviewSchema } = require('../schemas');
-const Review = require('../models/review');
+const { campgroundSchema } = require('../schemas');
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
@@ -18,93 +17,49 @@ const validateCampground = (req, res, next) => {
     }
 }
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
-
-/* GET home page. */
-router.get('/', (req, res, next) => {
-    res.render('home')
-});
-
 /* GET campgrounds index page. */
-router.get('/campgrounds', catchAsync(async (req, res, next) => {
+router.get('/', catchAsync(async (req, res, next) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 }));
 
 /* GET new campground page. CREATE */
-router.get('/campgrounds/new', (req, res, next) => {
+router.get('/new', (req, res, next) => {
     res.render('campgrounds/new');
 });
 
 /* POST new campground page. CREATE */
-router.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash('success', 'Successfully made a new campground');
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
 /* GET campground using id page. READ */
-router.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
+router.get('/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
-    console.log(campground);
     res.render('campgrounds/show', { campground });
 }));
 
 /* GET campground edit page. */
-router.get('/campgrounds/:id/edit', catchAsync(async (req, res, next) => {
+router.get('/:id/edit', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
 }));
 
 /* PUT campground edit page. UPDATE */
-router.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
+router.put('/:id', validateCampground, catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
 /* DELETE campground delete page. DELETE */
-router.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
+router.delete('/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }));
-
-/* POST new review page. CREATE */
-router.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-/* DELETE review delete page. DELETE */
-router.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res, next) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${ id }`);
-}));
-
-/* 404 for any incorrect page. */
-router.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404));
-});
-
-router.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err });
-});
 
 module.exports = router;
